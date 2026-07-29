716c3263b37e114839123956413071537da6b8b4
Dara <dara.esra28@gmail.com>
2026-07-29 16:10:42 -0600

    update PopupWithForm.ts

--- src/components/PopupWithForm.ts
+++ src/components/PopupWithForm.ts
@@ -1,43 +1,43 @@
-import { Popup } from "./Popup.js";
-import type { FormValues, HandleFormSubmitFunction } from "../types/types.js";
+import { Popup } from './Popup.js';
+import type { FormValues, HandleFormSubmitFunction } from '../types/types.js';
 
 export class PopupWithForm extends Popup {
   private formElement: HTMLFormElement;
   private inputList: NodeListOf<HTMLInputElement>;
   private handleFormSubmit: HandleFormSubmitFunction;
 
-    constructor(popupSelector: string, handleFormSubmit: HandleFormSubmitFunction) {
+  constructor(
+    popupSelector: string,
+    handleFormSubmit: HandleFormSubmitFunction
+  ) {
     super(popupSelector);
     this.handleFormSubmit = handleFormSubmit;
     this.formElement = this.popupElement.querySelector(
-      ".popup__form",
+      '.popup__form'
     ) as HTMLFormElement;
-    this.inputList = this.formElement.querySelectorAll("input");
+    this.inputList = this.formElement.querySelectorAll('input');
   }
 
   private getInputValues(): FormValues {
     const values: FormValues = {};
     this.inputList.forEach((input) => {
-        values[input.name] = input.value;
+      values[input.name] = input.value;
     });
     return values;
   }
 
   setEventListeners(): void {
     super.setEventListeners();
-    this.formElement.addEventListener("submit", (evt: SubmitEvent) => {
-        evt.preventDefault();
-        this.handleFormSubmit(this.getInputValues());
-        this.close();
+    this.formElement.addEventListener('submit', (evt: SubmitEvent) => {
+      evt.preventDefault();
+      this.handleFormSubmit(this.getInputValues());
     });
   }
 
   close(shouldClearForm: boolean = true): void {
     if (shouldClearForm) {
-        this.formElement.reset();
+      this.formElement.reset();
     }
     super.close(shouldClearForm);
   }
 }
-
-
